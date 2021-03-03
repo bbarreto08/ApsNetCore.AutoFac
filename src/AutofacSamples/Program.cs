133c05d659805e8d64a2288b15d3d3696e28c8c3
Bruno <brunocbarreto2012@gmail.com>
2021-03-03 07:56:18 -0300

    Commit inicial

---
+++ src/AutofacSamples/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace AutofacSamples
+{
+    public interface ILog
+    {
+        void Write(string message);
+    }
+
+    public class ConsoleLog : ILog
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Engine
+    {
+        private int id;
+        private ILog log;
+
+        public Engine(ILog log)
+        {
+            this.id = new Random().Next();
+            this.log = log;
+        }
+
+        public void Ahead(int power)
+        {
+            log.Write($"Engine [{id}] ahead {power}");
+        }
+    }
+
+    public class Car
+    {
+        private ILog log;
+        private Engine engine;
+
+        public Car(Engine engine, ILog log)
+        {
+            this.log = log;
+            this.engine = engine;
+        }
+
+        public void Go()
+        {
+            engine.Ahead(100);
+            log.Write("Car goiig forward...");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
